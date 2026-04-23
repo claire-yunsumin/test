b14af4066e34a1020cefb1ee0ccab59e9bdb49f7
claire-yunsumin <93638007+claire-yunsumin@users.noreply.github.com>
2026-04-23 11:16:07 +0900

    Initialize repository

--- src/app/dashboard/[taskId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import TaskDetailClient from "@/components/TaskDetailClient";
-
-export default function TaskDetailPage({
-  params,
-}: {
-  params: { taskId: string };
-}) {
-  return <TaskDetailClient taskId={params.taskId} />;
-}
